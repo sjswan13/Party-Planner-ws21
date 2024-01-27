532faea6b3051884735fea99af1e0460082ff530
sjswan13 <sjswan13@gmail.com>
2024-01-26 17:13:44 -0700

    rsvp/guest list

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const COHORT = "2109-CPU-RM-WEB-PT";
 const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`;
+const API_URL_RSVP = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/rsvps`;
+const API_URL_GUESTS = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/guests`;
 
 const state = {
   parties: [],
@@ -8,7 +10,12 @@ const state = {
 const partyList = document.querySelector('#party');
 
 const addPartyForm = document.querySelector('#addParty');
+console.log(addPartyForm, 'donkey')
 addPartyForm.addEventListener('submit', addParty);
+
+const addGuestSubmit = document.getElementById('guestSubmit');
+console.log(addGuestSubmit, 'fooo')
+addGuestSubmit.addEventListener('click', addGuestInfo);
 
 //sync state with the API and rerender
 
@@ -124,6 +131,33 @@ async function deleteParty(id) {
     console.log(error);
   }
 }
+
+async function showGuestList() {
+  try {
+    const response = await fetch(`${API_URL_GUESTS}`, {
+      method: 'GET',
+    });
+    if(!response.ok) {
+      throw new Error('Guest List Cannot Be Displayed');
+    }
+    const responseData = await response.json();
+    console.log('guest', responseData)
+
+    if(!Array.isArray(responseData.data)) {
+      throw new Error("Response Data is not an array");
+    }
+
+    const guestList = document.querySelector('#guests')
+    responseData.data.forEach(guest => {
+
+      const li = document.createElement('li');
+      li.textContent = guest.name;
+      guestList.appendChild(li);
+    });
+  }catch (error) {
+    console.error(error);
+  }
+}
 //render parties from state
 
 function renderParties() {
@@ -133,14 +167,36 @@ function renderParties() {
   }
 
   const partyCards = state.parties.map((party) => {
-    const partyCard = document.createElement('li');
+    const partyCard = document.createElement('div');
     partyCard.classList.add('party');
+
     partyCard.innerHTML = `
     <h2>${party.name}<h2>
     <h3>${party.date}<h3>
     <h3>${party.location}<h3>
     <p>${party.description}<p>
     `;
+    
+    const rsvpDropdown = document.createElement('select');
+
+    const rsvpOption = document.createElement('option');
+    rsvpDropdown.appendChild(rsvpOption);
+
+    ['RSVP','Attending', 'Not Attending', 'Maybe Attending'].forEach(option => {
+      const optionElement = document.createElement("option");
+      optionElement.value = option;
+      optionElement.textContent = option;
+      if(option === 'RSVP') {
+        optionElement.disabled = true;
+        optionElement.selected = true;
+      }
+      rsvpDropdown.appendChild(optionElement);
+    });
+     
+
+    rsvpDropdown.addEventListener('change', (event) => handleRSVPWithGuestInfo(party.id, event.target.value));
+
+    partyCard.appendChild(rsvpDropdown);
 
     //when createElement is used, have to also addEventListener
     const deleteButton = document.createElement('button');
@@ -148,9 +204,68 @@ function renderParties() {
     partyCard.append(deleteButton);
     deleteButton.addEventListener('click', () => deleteParty(party.id));
 
+    const showGuestListButton = document.createElement('button');
+    showGuestListButton.textContent = 'Guest List'
+    partyCard.append(showGuestListButton);
+    showGuestListButton.addEventListener('click', () => showGuestList());
+
     return partyCard;
   });
   partyList.replaceChildren(...partyCards);
 }
+
+async function handleRSVPWithGuestInfo(partyId, rsvpStatus) {
+  const guestForm = document.getElementById('guestForm');
+
+  guestForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+  });
+
+  if(rsvpStatus === 'Attending' || rsvpStatus === 'Maybe Attending') {
+    guestForm.style.display = 'block';
+
+  } else {
+    guestForm.style.display = 'none';
+  }
+  //await updateRSVP(partyId, rsvpStatus);
+}
+
+async function addGuestInfo(partyId) {
+  console.log('foo')
+  try {
+    const guestName = document.getElementById('guestName').value;
+    const guestEmail = document.getElementById('guestEmail').value;
+    const guestPhone = document.getElementById('guestPhone').value;
+
+    const response = await fetch(API_URL_GUESTS, {
+      method: "POST",
+      headers: {'Content-Type': "application/json"},
+      body: JSON.stringify({name: guestName, email: guestEmail, phone: guestPhone}),
+    });
+    console.log(response);
+  } catch(error) {
+    console.error(error);
+  }
+}
+
+async function updateRSVP(partyId, rsvpStatus) {
+  
+  try {
+    const response = await fetch(`${API_URL_RSVP}/${partyId}`, {
+      method: 'PUT',
+      headers: {'Content-Type': "application/json"},
+      body: JSON.stringify({rsvp: rsvpStatus})
+    });
+    if (!response.ok) {
+      throw new Error('Failed to update RSVP');
+    }
+    render();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+
 
 
